3f0a56ff978c3f366ae2a7b02f0f47f25dd96f10
Vladimir <vladimir@MacBook-Pro-Vladimir.local>
2017-01-20 23:09:37 +0200

    20.01.2017

--- crmsystem_frontend/src/main/java/net/proselyte/crmsystem/controller/TaskController.java
+++ crmsystem_frontend/src/main/java/net/proselyte/crmsystem/controller/TaskController.java
@@ -1,6 +1,7 @@
 package net.proselyte.crmsystem.controller;
 
 import net.proselyte.crmsystem.model.Task;
+import net.proselyte.crmsystem.model.User;
 import net.proselyte.crmsystem.service.TaskService;
 import net.proselyte.crmsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,28 +34,96 @@ public class TaskController {
         return "task/tasks";
     }
 
-    @RequestMapping("taskdata/{id}")
+    @RequestMapping(value="taskdata/{id}/", method = RequestMethod.GET)
     public String taskData(@PathVariable("id") Long id, Model model){
         model.addAttribute("task", this.taskService.getById(id));
         return "task/taskdata";
     }
 
-    @RequestMapping("removetask/{id}")
+    @RequestMapping("removetask/{id}/")
     public String removeTask(@PathVariable("id") Long id){
         this.taskService.remove(taskService.getById(id));
-        return "redirect:/task";
+        return "redirect:/task/";
     }
 
-    @RequestMapping(value = "/task/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/task/add/", method = RequestMethod.POST)
     public String taskSubmit(@ModelAttribute Task task){
         this.taskService.save(task);
-        return "redirect:/task";
+        return "redirect:/edittask/"+task.getId()+"/";
     }
 
-    @RequestMapping(value = "/task/add", method = RequestMethod.GET)
+    @RequestMapping(value = "/task/add/", method = RequestMethod.GET)
     public String addTask(Model model) {
         model.addAttribute("task", new Task());
+        model.addAttribute("listtasks", this.userService.getAll());
+        model.addAttribute("user", new User());
         return "task/taskadd";
     }
 
+    @RequestMapping(value = "/edittask/{id}")
+    public String editTask(@PathVariable("id") Long id, Model model){
+        model.addAttribute("task", this.taskService.getById(id));
+        model.addAttribute("listUsers", this.userService.getAll());
+        //model.addAttribute("user", new User());
+        return "task/taskadd";
+    }
+
+    @RequestMapping(value = "/edittask/{id}", method = RequestMethod.POST)
+    public String editSubmit(@PathVariable("id") Long id,
+                             @ModelAttribute Task task){
+        task.setId(id);
+        task.setResponsibleUser(this.taskService.getById(id).getResponsibleUser());
+        this.taskService.save(task);
+        return "redirect:/task/";
+    }
+
+    @RequestMapping(value = "/addresponsible/{userId}/{taskId}/", method = RequestMethod.GET)
+    public String addResponsibleUser(@PathVariable("userId") Long userId,
+                                     @PathVariable("taskId") Long taskId,
+                                     @ModelAttribute Task task,
+                                     Model model){
+        task = this.taskService.getById(taskId);
+        /*ArrayList<User> usersTemp = (ArrayList<User>) this.userService.getAll();
+        for (User user : usersTemp){
+            if (company.getResponsibleUser().contains(user)){
+                usersTemp.remove(user);
+            }
+        }*/
+        task.setResponsibleUser(this.userService.getById(userId));
+        this.taskService.save(task);
+        model.addAttribute("listTasks", this.userService.getAll());
+        model.addAttribute("task", this.taskService.getById(taskId));
+        return "task/taskadd";
+    }
+
+    @RequestMapping(value = "/addresponsible/{userId}/{taskId}/", method = RequestMethod.POST)
+    public String addResponsibleUserSubmit(@PathVariable("taskId") Long taskId,
+                                           @ModelAttribute Task task){
+        task.setId(taskId);
+        task.setResponsibleUser(this.taskService.getById(taskId).getResponsibleUser());
+        this.taskService.save(task);
+        return "redirect:/task/";
+    }
+
+    @RequestMapping(value = "/removeresponsible/{userId}/{taskId}/", method = RequestMethod.GET)
+    public String removeResponsibleUser(@PathVariable("userId") Long userId,
+                                        @PathVariable("taskId") Long taskId,
+                                        @ModelAttribute Task task,
+                                        Model model){
+        task = this.taskService.getById(taskId);
+        task.removeResponsibleUser(this.userService.getById(userId));
+        this.taskService.save(task);
+        model.addAttribute("listTasks", this.userService.getAll());
+        model.addAttribute("task", this.taskService.getById(taskId));
+        return "task/taskadd";
+    }
+
+    @RequestMapping(value = "/removeresponsible/{userId}/{taskId}/", method = RequestMethod.POST)
+    public String removeResponsibleUserSubmit(@PathVariable("taskId") Long taskId,
+                                              @ModelAttribute Task task){
+        task.setId(taskId);
+        task.setResponsibleUser(this.taskService.getById(taskId).getResponsibleUser());
+        this.taskService.save(task);
+        return "redirect:/task/";
+    }
 }
